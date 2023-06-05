f4b9d352580a797c01091faabf01570f5eb5eafc
Basant-Adel <basantadel99@gmail.com>
2023-06-05 16:30:21 -0700

    9. Easter Egg

---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+str = "The Zen of Python, by Tim Peters\n\nBeautiful is better than ugly.\n\
+Explicit is better than implicit.\nSimple is better than complex.\n\
+Complex is better than complicated.\nFlat is better than nested.\n\
+Sparse is better than dense.\nReadability counts.\n\
+Special cases aren't special enough to break the rules.\nAlthough\
+practicality beats purity.\nErrors should never pass silently.\n\
+Unless explicitly silenced.\nIn the face of ambiguity, refuse the\
+temptation to guess.\nThere should be one-- and preferably only\
+one --obvious way to do it.\nAlthough that way may not be obvious\
+at first unless you're Dutch.\nNow is better than never.\n\
+Although never is often better than *right* now.\n\
+If the implementation is hard to explain, it's a bad idea.\n\
+If the implementation is easy to explain, it may be a good idea.\n\
+Namespaces are one honking great idea -- let's do more of those!"
+print(str)
