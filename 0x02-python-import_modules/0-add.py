2cd2e3a630cd58504ce7878c610b8ef65dfaf45f
Basant-Adel <basantadel99@gmail.com>
2023-06-08 10:00:24 -0700

    0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+"""
+if-name=main-> To only run the code inside the if statement,
+When the program is run directly by the Python interpreter.
+From..import..->when u need 2 use its code many times in your code project.
+"""
