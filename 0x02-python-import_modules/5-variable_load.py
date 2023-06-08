d8ef424b327067a3aba7c6e72756c0292bd2f7e8
Basant-Adel <basantadel99@gmail.com>
2023-06-08 13:13:11 -0700

    5-variable_load.py

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# It prints the value, which it's in the file
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print(a)
+"""
+if-name=main-> To only run the code inside the if statement,
+When the program is run directly by the Python interpreter.
+From..import..->when u need 2 use its code many times in your code project.
+"""
