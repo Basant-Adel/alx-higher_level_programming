de9b4a3871a5e6b9a13ebcaae82adb88dfad44cd
Basant-Adel <basantadel99@gmail.com>
2023-06-12 10:30:01 -0700

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,6 @@
 def no_c(my_string):
     no_c_string = ""
     for ch in my_string:
-        if ch == 'c' or ch == 'C':
-            continue
-        else:
+        if ch != 'c' and ch != 'C':
             no_c_string += ch
     return no_c_string
