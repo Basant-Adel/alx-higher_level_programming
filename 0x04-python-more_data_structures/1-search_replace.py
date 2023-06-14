4c21b250c2561ce756be60c7fdf357804e27b093
Basant-Adel <basantadel99@gmail.com>
2023-06-14 15:45:28 -0700

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    n_list = []
+    for n in my_list:
+        if n == search:
+            n_list.append(replace)
+        else:
+            n_list.append(n)
+    return n_list
