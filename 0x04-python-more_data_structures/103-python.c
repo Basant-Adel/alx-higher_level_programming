791902097d41afe40eb8c256fc8b2f55a71b9a68
Basant-Adel <basantadel99@gmail.com>
2023-06-14 17:10:31 -0700

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,35 +1,31 @@
 #include <Python.h>
 #include <stdio.h>
-
 /**
  * print_python_list - adds a new node at the end of a listint_t list
- *@p: pointer PyObject
+ * @p: pointer PyObject
  */
-
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t size, ba;
+	Py_ssize_t size, a;
 	PyObject *item;
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
 	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
-	for (ba = 0; ba < PyList_Size(p); ba++)
+	for (a = 0; a < PyList_Size(p); a++)
 	{
-		item = PyList_GetItem(p, ba);
-		printf("Element %ld: %s\n", ba, Py_TYPE(item)->tp_name);
+		item = PyList_GetItem(p, a);
+		printf("Element %ld: %s\n", a, Py_TYPE(item)->tp_name);
 	}
 }
-
 /**
  * print_python_bytes - adds a new node at the end of a listint_t list
- *@p: pointer PyObject
+ * @p: pointer PyObject
  */
-
 void print_python_bytes(PyObject *p)
 {
-	Py_ssize_t size, ba;
+	Py_ssize_t size, a;
 	char *bytes_str;
 
 	printf("[.] bytes object info\n");
@@ -47,9 +43,9 @@ void print_python_bytes(PyObject *p)
 	printf("  trying string: %s\n", bytes_str);
 
 	printf("  first %ld bytes:", size + 1);
-	for (ba = 0; ba < size + 1 && ba < 10; ba++)
+	for (i = 0; a < size + 1 && a < 10; a++)
 	{
-		printf(" %.2x", bytes_str[ba] & 0xFF);
+		printf(" %.2x", bytes_str[a] & 0xFF);
 	}
 	printf("\n");
 }
