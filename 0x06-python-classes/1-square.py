04d92bba0ff8bb5b0fb0741f0bd37bd836fda522
Basant-Adel <basantadel99@gmail.com>
2023-06-27 14:51:04 -0700

    Square with size :)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Defines a square """
+
+
+class Square:
+    """ Square """
+
+    def __init__(self, size=0):
+        """ Private instance attribute: size """
+        self.__size = size
