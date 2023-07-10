45547b20b9c6145c4522336ad99a005bdb4ab1f4
Basant-Adel <basantadel99@gmail.com>
2023-07-10 09:42:46 -0700

    Soooo Inherits From :)

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,4 +11,4 @@ def inherits_from(obj, a_class):
     Inherits From
     """
 
-    return (issubclass(type(obj), a_class) and type(obj) != a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class
