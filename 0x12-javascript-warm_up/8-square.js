3404a8c773f7ddf808b60fccd92952822dfd9ac5
Basant-Adel <basantadel99@gmail.com>
2023-09-11 13:12:42 -0700

    Square :D

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Write a script that prints a square
+
+const size = Math.floor(Number(process.argv[2]));
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let b = 0; b < size; b++) {
+    let so = '';
+    for (let n = 0; n < size; n++) so += 'X';
+    console.log(so);
+  }
+}
