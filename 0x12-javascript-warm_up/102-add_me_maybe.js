79aea44dffdcd8e960a5f09fcee79e5f8998fc3f
Basant-Adel <basantadel99@gmail.com>
2023-09-11 14:06:54 -0700

    Add Me Maybe :D

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Write a function that increments and calls a function
+// The function must be visible from outside
+
+exports.addMeMaybe = function (number, theFunction) {
+  theFunction(++number);
+};
