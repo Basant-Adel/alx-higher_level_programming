e297a3dd8cb82713a49b1eff1780e699d7094a80
Basant-Adel <basantadel99@gmail.com>
2023-10-24 15:02:35 -0700

    Write me :D

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Write a script that writes a string to a file
+// The first argument is the file path
+
+const fs = require('fs');
+
+if (process.argv.length > 3) {
+  fs.writeFile(process.argv[2], process.argv[3], (error) => {
+    if (error) console.log(error);
+  });
+}
