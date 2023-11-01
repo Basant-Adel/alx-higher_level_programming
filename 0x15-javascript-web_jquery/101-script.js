f1d7515a67bf90b5a8c5b1c75e2ad5a301b173e4
Basant-Adel <37676351+Basant-Adel@users.noreply.github.com>
2023-11-02 01:31:52 +0200

    List & Add & Remove :D

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,12 @@
+// To add, remove and clear LI elements from list when user clicks
+$('document').ready(function () {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('DIV#remove_item').click(function () {
+    $('UL.my_list li:last').remove();
+  });
+  $('DIV#clear_list').click(function () {
+    $('UL.my_list').empty();
+  });
+});
