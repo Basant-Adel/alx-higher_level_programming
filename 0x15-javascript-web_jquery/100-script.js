cf70bc316f916e0ff7ccbf33f70219b2684ba594
Basant-Adel <37676351+Basant-Adel@users.noreply.github.com>
2023-11-02 01:27:04 +0200

    No jQuery - Document Loaded :D

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,4 @@
+// Script to update text color of <header> element to red (#FF0000)
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('header').style.color = '#FF0000';
+});
